62a42e32b6620dd74fee9a98e5903b648259bc89
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-06-17 16:43:01 -0300

    Borre el archivo database

--- database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/chat', {
-    useNewUrlParser: true
-})
-  .then(db => console.log('Base de datos conectada'))
-  .catch(err => console.log(err));
